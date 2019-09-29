610566eab5aa92ac1f2608999932e1c79f67e061
agnitio247 <agnitio247@protonmail.com>
2019-09-29 20:50:00 +0000

    exemple

--- src/bsc.cpp
+++ src/bsc.cpp
@@ -9,6 +9,43 @@ using namespace std;
 #include "../include/Scheduler.h"
 
 int main(int argc, char const *argv[]) {
-  
-  return 0;
+	string currentDayTime[] = {
+		"20:25:52",
+	};
+	schedule currentDay = {
+		"Sunday",
+		currentDayTime,
+		1
+	};
+
+	string fridayTime[] = {
+		"08:30:00",
+		"12:00:15"
+	};
+	schedule friday = {
+		"Friday",
+		fridayTime,
+		2
+	};
+
+	string wednesdayTime[] = {
+		"08:30:00",
+		"13:30:00",
+		"14:00:20"
+	};
+	schedule wednesday = {
+		"Wednesday",
+		wednesdayTime,
+		3
+	};
+
+	schedule backups[] = {
+		friday,
+		wednesday
+	};
+
+	int backupsSize = sizeof(backups)/sizeof(backups[0]);
+
+	schedule nextBackup = GetNextBackup(currentDay, backups, backupsSize);
+	return 0;
 }
